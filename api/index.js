636e672c93b132248e654cd9b5c71b468ba8acfa
chilunhay <72005042+chilunhay@users.noreply.github.com>
2026-03-08 13:50:20 +0700

    Fix database connection for serverless

--- api/index.js
+++ api/index.js
@@ -1,2 +1,14 @@
 const app = require("../backend/app");
+const connectDatabase = require("../backend/db/Database");
+const cloudinary = require("cloudinary");
+
+// Chỉ khởi tạo khi chạy trên môi trường serverless
+connectDatabase();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 module.exports = app;
